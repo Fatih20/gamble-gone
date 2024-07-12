66fff5383309b1159a706861002cb6385c34995c
Fatih20 <fnri39@protonmail.com>
2024-07-12 18:55:22 +0700

    feat: better conversation

--- web/lib/ai/chatbot.ts
+++ web/lib/ai/chatbot.ts
@@ -5,7 +5,10 @@ import { UpstashVectorStore } from "@langchain/community/vectorstores/upstash";
 import { DocumentInterface } from "@langchain/core/documents";
 import { BaseLanguageModelInput } from "@langchain/core/language_models/base";
 import { HumanMessage, AIMessageChunk } from "@langchain/core/messages";
-import { StringOutputParser } from "@langchain/core/output_parsers";
+import {
+  JsonOutputParser,
+  StringOutputParser,
+} from "@langchain/core/output_parsers";
 import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import {
   Runnable,
@@ -20,6 +23,11 @@ import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { createRetrieverTool } from "langchain/tools/retriever";
 import { traceable } from "langsmith/traceable";
 import { wrapSDK } from "langsmith/wrappers";
+
+interface QuestionClassification {
+  question: string;
+  sentiment: "venting" | "talk" | "question";
+}
 
 const promptString = {
   questionRefinement: `
@@ -44,14 +52,37 @@ const promptString = {
   
   Respond ONLY with the augmented question:
   `,
-  answer: `
-  You are a therapist that is tasked to help people recovering from online gambling addiction. Those recovering addict will ask you questions in their path to recovery.
-  -------
-  One of those recovering online gambling addict just asked you a question.
-
-  The online gambling addict's question is the following:
+  questionClassification: `
+  You are a bot responsible to do sentiment analysis on a statement given by a person. You have been given the following statement:
   {question}
 
+  -----
+  You may classify the statement into three type:
+  - "venting" // the person is just telling you about their struggle
+  - "question" // the person asked you something
+  - "talk" // the person is just making small talk
+  
+  If the statement is a "question", rephrase and reword that question to be more expansive, but keep it true to the question and still be succinct.
+
+  Output strictly only the following JSON format:
+  {{
+    "question" : string // if the statement was a question, put the processed question here. Otherwise, put in the given statement VERBATIM WITHOUT ANY MODIFICATION.
+    "sentiment" : string // the classification for the statement that you have given
+  }}
+
+  ----
+  `,
+  answer: `
+  You are a therapist that is tasked to help people recovering from online gambling addiction. Those recovering addict will talk to you in their path to recovery.
+  -------
+  Your input #1 is what the gambler you're treating has said to you:
+  {question}
+
+  Your input #2 is the sentiment classification of what has been said by the gambler:
+  {sentiment}
+
+  The sentiment could only be three of the following: "venting", "question", "talk".
+  -------
   The addict's background is as follows:
   {gamblingStory}
 
@@ -60,11 +91,20 @@ const promptString = {
   The addict's wanted to stop because of the following reason:
   {whyStop}
   -------
+  The way that you answer the question is dependent on the your input #2, the sentiment.
+
+  # Case 1: sentiment is "venting"
+  Just lend your ear to the gambler. You don't need to put forth any factual information. Give advice ONLY WHEN appropriate. 
+
+  # Case 2: sentiment is "talk"
+  Just respond with the gambler normally as another person. For example, when they said "Hi", you may respond with a "Hello".
+
+  # Case 3: sentiment is "question"
   Answer that question using the following excerpts from various documents that are relevant to the question:
   {context}
 
   -------
-  Act as a helpful and patient therapist with years of experience who understands clearly the suffering of that recovering gambler. You may only answer as long as 5 sentences. 
+  In doing your task, you must act as a helpful and patient therapist with years of experience who understands clearly the suffering of that recovering gambler. You may only answer as long as 5 sentences.
 
   If you do not know the answer, answer "Sorry, I cannot answer that question".
 
@@ -81,13 +121,18 @@ export type ChatParameter = {
 
 export type RefinementOutput = ChatParameter & {
   context: DocumentInterface<Record<string, any>>[];
+  sentiment: string;
 };
 
 export class Chatbot {
   private static instance: Chatbot;
   private store: UpstashVectorStore;
   private retriever: VectorStoreRetriever<UpstashVectorStore>;
-  private refinementChain: Runnable<ChatParameter, string, RunnableConfig>;
+  private classificationChain: Runnable<
+    ChatParameter,
+    QuestionClassification,
+    RunnableConfig
+  >;
   private composedChain?: Runnable<ChatParameter, string, RunnableConfig>;
   private answerChain?: RunnableSequence<Record<string, unknown>, string>;
 
@@ -118,7 +163,7 @@ export class Chatbot {
 
     console.log("Constructing LLM");
 
-    const llmRefinement = wrapSDK(
+    const llmClassification = wrapSDK(
       new ChatAnthropic({
         model: "claude-3-5-sonnet-20240620",
         temperature: 0.4,
@@ -127,17 +172,17 @@ export class Chatbot {
 
     console.log("Constructing prompt template");
 
-    const refinementPrompt = PromptTemplate.fromTemplate(
-      promptString.questionRefinement,
+    const classificationPrompt = PromptTemplate.fromTemplate(
+      promptString.questionClassification,
     );
 
     console.log("Constructing chains");
 
-    const refinementChain = refinementPrompt
-      .pipe(llmRefinement)
-      .pipe(new StringOutputParser());
+    const classificationChain = classificationPrompt
+      .pipe(llmClassification)
+      .pipe(new JsonOutputParser<QuestionClassification>());
 
-    this.refinementChain = refinementChain;
+    this.classificationChain = classificationChain;
   }
 
   static async getInstance() {
@@ -169,15 +214,16 @@ export class Chatbot {
 
     const composedChain = new RunnableLambda({
       func: async (input: ChatParameter) => {
-        const result = await this.refinementChain.invoke(input);
+        const result = await this.classificationChain.invoke(input);
         const { gamblingDuration, gamblingStory, whyStop } = input;
-        const retrievedDocs = await this.retriever.invoke(result);
+        const retrievedDocs = await this.retriever.invoke(result.question);
         return {
-          question: result,
+          question: result.question,
           gamblingStory,
           gamblingDuration,
           whyStop,
           context: retrievedDocs,
+          sentiment: result.sentiment,
         } as RefinementOutput;
       },
     }).pipe(this.answerChain);
